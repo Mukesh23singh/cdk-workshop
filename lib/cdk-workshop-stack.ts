d3265f33016f986866fdd82f78de06249cd2fdce
Mukesh Singh <mukesh23.singh@gmail.com>
2021-04-13 14:44:27 +0530

    added outputs to stacks

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,31 +1,44 @@
-import * as lambda from "@aws-cdk/aws-lambda";
-import * as apigw from "@aws-cdk/aws-apigateway";
-import * as cdk from "@aws-cdk/core";
-import { HitCounter } from "./hitcounter";
-import { TableViewer } from "cdk-dynamo-table-viewer";
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as path from 'path';
+import { HitCounter } from './hitcounter';
+import { TableViewer } from 'cdk-dynamo-table-viewer';
 
 export class CdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-    // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, "HelloHandler", {
-      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
-      code: lambda.Code.fromAsset("lambda"), // code loaded from "lambda" directory
-      handler: "hello.handler", // file is "hello", function is "handler"
+  public readonly hcViewerUrl: cdk.CfnOutput;
+  public readonly hcEndpoint: cdk.CfnOutput;
+
+  constructor(scope: cdk.Construct, is: string, props?: cdk.StackProps) {
+    super(scope, is, props);
+
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      code: lambda.Code.asset(path.resolve(__dirname, '../lambda')),
+      handler: 'hello.handler',
     });
 
-    const helloWithCounter = new HitCounter(this, "HelloHitCounter", {
-      downstream: hello,
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
-    new apigw.LambdaRestApi(this, "Endpoint", {
-      handler: helloWithCounter.handler,
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
     });
 
-    new TableViewer(this, "ViewHitCounter", {
-      title: "Hello Hits",
+    const tv = new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
       table: helloWithCounter.table,
+      sortBy: '-hits'
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
+    this.hcViewerUrl = new cdk.CfnOutput(this, 'TableViewerUrl', {
+      value: tv.endpoint
     });
   }
 }
